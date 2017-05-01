1598bcf836308be9760536fa1598d9e1674d15aa
marcstoehr <marc.stoehr@uni-leipzig.de>
2017-05-01 17:10:28 +0200

    final commit for removing hard coded password from .sh and .sql files

--- bootstrap.sh
+++ bootstrap.sh
@@ -62,8 +62,6 @@ a2enmod cgi
 
 # install mysql and java and perl clients
 # ATTENTION: CUSTOMISE THE MYSQL PASSWORD FOR YOUR OWN INSTALLATION !!!
-# debconf-set-selections <<< 'mysql-server mysql-server/root_password password bird2006'
-# debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password bird2006'
 debconf-set-selections <<< "mysql-server mysql-server/root_password password $PW"
 debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PW"
 apt-get install -y mysql-server mysql-client libdbd-mysql-perl
