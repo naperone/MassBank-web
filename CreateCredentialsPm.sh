2824d237fa8d05c48cb367fd1e870d2a350390da
marcstoehr <marc.stoehr@uni-leipzig.de>
2017-05-06 12:59:42 +0200

    implementation of dynamic creation of Credentials.pm from within the vagrant install script

--- CreateCredentialsPm.sh
+++ CreateCredentialsPm.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
 printf "use strict;
-package MassBank::Credentials;
-use base 'Exporter';
-our @EXPORT = {'getCredentials'}
+package Credentials;
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT = qw(getCredentials);
 
 sub getCredentials {
 \tmy @foo;
-\tmy $User;
-\tmy $PassWord;
+\tmy \$User;
+\tmy \$PassWord;
 \topen(F, \"$CREDPATH/password.sh\");
 \t@foo = grep(/USER/,<F>);
 \tclose(F);
@@ -23,8 +24,8 @@ sub getCredentials {
 \t\tchomp;
 \t\t\$PassWord = ((split(\"=\"))[1]);
 \t}
-\t%res = (\"PassWord\",\$PassWord,\"User\",\$User);
-\treturn \$res;
+\t%%res = (\"PassWord\",\$PassWord,\"User\",\$User);
+\treturn %%res;
 }
 
-1;" > Credentials.pm
+1;"
